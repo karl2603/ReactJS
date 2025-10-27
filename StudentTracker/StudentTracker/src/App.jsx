68447d21e191b4ab9d7724794f8ae1ed00e0de1f
karl2603 <arvindrajkarl@gmail.com>
2025-10-27 19:52:06 +0530

    add student function

--- StudentTracker/StudentTracker/src/App.jsx
+++ StudentTracker/StudentTracker/src/App.jsx
@@ -5,7 +5,9 @@ import Header from "./header.jsx";
 
 
 function App() {
-    const students = [
+    let displayStudents;
+
+    const [students,setStudents] = useState([
         { name: "Manikam", marks: 38 },
         { name: "Vilasni", marks: 40 },
         { name: "Radesh", marks: 16 },
@@ -16,11 +18,10 @@ function App() {
         { name: "Roshan", marks: 21 },
         { name: "Rupashree", marks: 46 },
         { name: "Vishwha", marks: 20 }
-    ]
-
-    let displayStudents;
-
+    ])
     const [filter, setFilter] = useState("All")
+    const [name,setName] = useState("")
+    const [mark,setMark] = useState("")
 
     function allStudents() {
         setFilter("All")
@@ -34,14 +35,34 @@ function App() {
         setFilter("Fail")
     }
 
-    if(filter === "All"){
+    function addName(event){
+        setName(event.target.value)
+    }
+
+    function addMark(event){
+        setMark(event.target.value)
+    }
+
+    function addStudents() {
+      let newStudent = {
+        id : Date.now(),
+        name : name,
+        marks : Number(mark)
+      }
+      console.log(newStudent)
+      setStudents([...students, newStudent])
+      setName("")
+      setMark("")
+    }
+
+    if (filter === "All") {
         displayStudents = students;
     }
-    else if(filter === "Pass"){
-        displayStudents = students.filter( students => students.marks>=25)
+    else if (filter === "Pass") {
+        displayStudents = students.filter(students => students.marks >= 25)
     }
-    else if(filter === "Fail"){
-        displayStudents = students.filter( students => students.marks<25)
+    else if (filter === "Fail") {
+        displayStudents = students.filter(students => students.marks < 25)
     }
 
     return (
@@ -52,16 +73,29 @@ function App() {
                 <button className="btn passbtn" onClick={passedStudents}>Passed</button>
                 <button className="btn failbtn" onClick={failedStudents}>Failed</button>
             </div>
+            <div className="inputContainer">
+                <div className="nameDiv">
+                    <span className="studentName">Name</span>
+                    <input className="studentNameInput" placeholder="Enter your name..." type="text" onChange={addName} value={name}></input>
+                </div>
+                <div className="markDiv">
+                    <span className="studentMark">Mark</span>
+                    <input className="studentMarkInput" placeholder="Enter marks..." type="number" onChange={addMark} value={mark}></input>
+                </div>
+                <div className="addBtnDiv">
+                    <button className="addbtn" onClick={addStudents}>Add</button>
+                </div>
+            </div>
             <div className="displayContainer">
-            {
-                displayStudents.map( (student) => (
-                    <div className="StudentContainer" style={{backgroundColor : student.marks>=25 ? "#D1FAE5" : "#FEE2E2"}}>
-                        <h3 className="name">{student.name}</h3>
-                        <h3 className="marks"> Marks : {student.marks}/50</h3>
-                    {student.marks >=25 ? (<h3 className="passText">Pass</h3>) : (<h3 className="failText">Fail</h3>)}
-                    </div>
-                ) )
-            }
+                {
+                    displayStudents.map((student) => (
+                        <div className="StudentContainer" style={{ backgroundColor: student.marks >= 25 ? "#D1FAE5" : "#FEE2E2" }}>
+                            <h3 className="name">{student.name}</h3>
+                            <h3 className="marks"> Marks : {student.marks}/50</h3>
+                            {student.marks >= 25 ? (<h3 className="passText">Pass</h3>) : (<h3 className="failText">Fail</h3>)}
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
