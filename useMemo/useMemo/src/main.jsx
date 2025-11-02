8579121ee434bae84ba5b829df7519d9446bea35
karl2603 <arvindrajkarl@gmail.com>
2025-11-02 09:39:53 +0530

    useMemo hook

--- useMemo/useMemo/src/main.jsx
+++ useMemo/useMemo/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { UseMemo } from './UseMemo.jsx';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <UseMemo />
   </StrictMode>,
 )
---
+++ useMemo/useMemo/src/useMemo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState } from 'react';
+import "./useMemo.css";
+
+function UseMemo() {
+  return (
+    <div>UseMemo</div>
+  )
+}
+
+export {UseMemo};
