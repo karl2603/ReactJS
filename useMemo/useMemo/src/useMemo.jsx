567f53fd2609645f42ff503c7a25a4d9a90b9e9e
karl2603 <arvindrajkarl@gmail.com>
2025-11-02 10:11:06 +0530

    UseMemo

--- useMemo/useMemo/src/useMemo.jsx
+++ useMemo/useMemo/src/useMemo.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import "./useMemo.css";
 
 function UseMemo() {
+  const [count, setCount] = useState(0);
+
+  const double = useMemo(() => {
+    return count * 2
+  }, [count]);
+
   return (
-    <div>UseMemo</div>
-  )
+    <div>
+      <p>Count: {count}</p>
+      <p>Double: {double}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
 }
 
-export {UseMemo};
+export { UseMemo };
