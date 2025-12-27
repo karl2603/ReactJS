7c529038363c964fe1236b8d829787a5582a94c0
karl2603 <arvindrajkarl@gmail.com>
2025-12-27 11:44:09 +0530

    react router setup

--- TaskTracker/TaskTracker/src/App.jsx
+++ TaskTracker/TaskTracker/src/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
-import { Home } from "./pages/Home.jsx";
-import { AddTask } from "./pages/AddTask.jsx";
-import { TaskDetails } from "./pages/TaskDetails.jsx";
+import { useState } from 'react';
+import  Home  from "./pages/Home.jsx";
+import  AddTask  from "./pages/AddTask.jsx";
+import  TaskDetails  from "./pages/TaskDetails.jsx";
 import "./App.css"
 
 function App() {
+  const [tasks,setTasks] = useState([]);
   return (
     <BrowserRouter >
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path="/Add" element={<AddTask />} />
-      <Route path='/Task/:id' element={<TaskDetails />} />
+      <Route path='/' element={<Home 
+      tasks={tasks}
+      />} />
+      <Route path="/add" element={<AddTask 
+      tasks={tasks}
+      setTasks={setTasks}
+      />} />
+      <Route path='/task/:id' element={<TaskDetails 
+      tasks={tasks}
+      />} />
     </Routes>
     </BrowserRouter>
   )
